485aab84a83a5bfe5e5c9a90fad371523036f19f
Peter Anh <webfullstack99@gmail.com>
2021-07-18 20:41:43 +0700

    save

--- lib/screens/auth/widgets/auth_title.dart
+++ lib/screens/auth/widgets/auth_title.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+
 class AuthTitle extends StatelessWidget {
   final String text;
   const AuthTitle({
-    Key? key, required this.text,
+    Key? key,
+    required this.text,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text(text, style: Theme.of(context).textTheme.headline4!);
+    bool isLightMode = Theme.of(context).brightness == Brightness.light;
+    return Text(text,
+        style: Theme.of(context)
+            .textTheme
+            .headline4!
+            .copyWith(color: isLightMode ? Theme.of(context).primaryColor: Colors.white));
   }
 }
